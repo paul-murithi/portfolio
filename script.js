eaaf8e5ea2dfa189aa497a2e2aa6f1cd7edcbbd8
paul-murithi <paulmurithi518@gmail.com>
2023-09-24 18:51:40 +0300

    Added responsiveness to the buttons.

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const redirectButtonTransitEase = document.querySelector('.js-transit-ease');
+const redirectButtonAmazon = document.querySelector('.js-amazon');
+const redirectButtonAirBnb = document.querySelector('.js-air-bnb');
+const redirectButtonFarmManagement = document.querySelector('.js-farm-management');
+
+
+redirectButtonTransitEase.addEventListener('click', function () {
+    window.open('https://github.com/paul-murithi/TransitEase-Bus-Travel-Website', '_blank');
+});
+
+redirectButtonAmazon.addEventListener('click', function () {
+    window.open('https://github.com/paul-murithi/Amazon-Ecommerce-clone', '_blank');
+});
+
+redirectButtonAirBnb.addEventListener('click', function () {
+    window.open('https://github.com/paul-murithi/AirBnB_clone_v2', '_blank');
+});
+
+redirectButtonFarmManagement.addEventListener('click', function () {
+    window.open('https://github.com/paul-murithi/Farm_managemnt_system', '_blank');
+});
