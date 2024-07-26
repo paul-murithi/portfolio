4273566da6baf2ef4463e6c4a5f57c0776f08ef0
paul-murithi <paulmurithi233@gmail.com>
2024-07-26 22:14:26 +0300

    Update CV link

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -4,7 +4,7 @@ import styles from "./Resume.module.css";
 export const Resume = () => {
   return (
     <a
-      href="https://drive.google.com/uc?export=download&id=18tISs_hA2wB6vefE7dnkMqHk7nS98wEA"
+      href="https://drive.google.com/uc?export=download&id=1H76FLPPUm_YGCQCsvjC7KHZNg8KSDx4a"
       download
       className={styles.link}
     >
