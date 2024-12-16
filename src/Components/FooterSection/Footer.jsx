3f9150bd41f4d43d8f5ee0db95814ddbacbcb96d
paul-murithi <paulmurithi233@gmail.com>
2024-12-16 11:49:28 +0300

    feat: Added contact form

--- src/Components/FooterSection/Footer.jsx
+++ src/Components/FooterSection/Footer.jsx
@@ -3,21 +3,17 @@ import styles from "./Footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import TitleHeading from "../TitleHeading/TitleHeading";
+import ContactForm from "../contact/ContactForm";
 
 export const Footer = () => {
   return (
     <footer className={styles.contact}>
       <TitleHeading headingName={"Contact Me"} />
-      <div className={styles.contactLinks}>
-        <div className="phone-no link">
-          <h4>Call me</h4>
+      <ContactForm />
+      <div className={styles.additionalLinks}>
+        <div>
+          <h4 className={styles.callLink}>Call me</h4>
           <a href="tel:+254748924732">+2547 4892 4732</a>
-        </div>
-        <div className="email link">
-          <h4>Email:</h4>
-          <a href="mailto:paulmurithi518@gmail.com" rel="noreferrer">
-            paulmurithi518@gmail.com
-          </a>
         </div>
         <div className={styles.socialMediaIcons}>
           <a
---
+++ src/Components/contact/ContactForm.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import { Button } from "../Button/Button";
+import styles from "./Contact.module.css";
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xnnqkqnn");
+  const [errors, setErrors] = useState({});
+
+  const validateFields = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const message = e.target.message.value;
+
+    const newErrors = {};
+    if (!email.includes("@")) newErrors.email = "Invalid email format.";
+    if (message.trim() === "") newErrors.message = "Message cannot be empty.";
+
+    if (Object.keys(newErrors).length === 0) {
+      handleSubmit(e);
+    } else {
+      setErrors(newErrors);
+      console.log(errors);
+    }
+  };
+
+  if (state.succeeded) {
+    return <p>Thanks for contacting me!</p>;
+  }
+
+  return (
+    <form className={styles.contactForm} onSubmit={validateFields}>
+      <label htmlFor="email" className={styles.formLabel}>
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        className={styles.input}
+        placeholder="Enter your Email Address"
+        required
+      />
+
+      <ValidationError
+        prefix="Email"
+        field="email"
+        errors={state.errors}
+        className={styles.validationError}
+      />
+
+      <label htmlFor="message" className={styles.formLabel}>
+        Enter Your Message
+      </label>
+      <textarea
+        id="message"
+        name="message"
+        className={styles.input}
+        placeholder="Enter Your Message"
+        required
+      />
+
+      <ValidationError
+        prefix="Message"
+        field="message"
+        errors={state.errors}
+        className={styles.validationError}
+      />
+      <button
+        type="submit"
+        disabled={state.submitting}
+        className={styles.submitButton}
+      >
+        Send
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
